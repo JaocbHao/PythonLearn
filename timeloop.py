1ae44ec81803134006da85725a541349345d6ff3
JaocbHao <58319356+JaocbHao@users.noreply.github.com>
2019-12-07 21:00:55 -0500

    Create timeloop.py

---
+++ timeloop.py
@@ -0,0 +1,10 @@
+# Output the entire wizard’s spell by counting from 1 to N, giving one number and “Abracadabra” per line
+
+x = input()
+i = 1
+#for number in range (int(x)):
+ # print(x + "Abracadabra")
+
+while i < int(x)+1:
+  print(i,"Abracadabra");
+  i = i+1;
